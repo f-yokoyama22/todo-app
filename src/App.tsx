9142f3777c0ae2b6c2a10e9778e6291220e8ba19
yokoyama <f-yokoyama@atware.co.jp>
2024-07-29 20:24:18 +0900

    Add auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css';
+import outputs from '../amplify_outputs.json';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import TodoPage from './features/TodoPage.tsx';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
 
 function App() {
   return (
     <>
-      <TodoPage />
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user?.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                    <TodoPage />
+                </main>
+            )}
+        </Authenticator>
     </>
   )
 }
